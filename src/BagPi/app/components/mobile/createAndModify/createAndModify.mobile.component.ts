0170b7e802840520acfec32b12b5b0f02ab06d8e
thomasslee97 <thomasslee97@gmail.com>
2016-09-13 16:10:45 +0100

    Moved Social Icons To Object In Controller

--- src/BagPi/app/components/mobile/createAndModify/createAndModify.mobile.component.ts
+++ src/BagPi/app/components/mobile/createAndModify/createAndModify.mobile.component.ts
@@ -8,7 +8,164 @@ import { BagPiScreenService } from '../../../services/index';
     styleUrls: ['views/components/mobile/createAndModify/createAndModify.mobile.component.css']
 })
 export class BagPiCreateAndModifyMobileComponent {
-    constructor(private screenService: BagPiScreenService) {
+    public iconList = [
+        {
+            "name": "Facebook",
+            "code": "&#xf09a;",
+            "styles": [
+                {
+                    "color": "#3b5998"
+                }
+            ]
+        },
+        {
+            "name": "Twitter",
+            "code": "&#xf099;",
+            "styles": [
+                {
+                    "color": "#1DA1F2"
+                }
+            ]
+        },
+        {
+            "name": "Soundcloud",
+            "code": "&#xf1be;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "Google+",
+            "code": "&#xf2b3;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "YouTube",
+            "code": "&#xf167;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "Instagram",
+            "code": "&#xf16d;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "GitHub",
+            "code": "&#xf09b;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "Slack",
+            "code": "&#xf198;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "Reddit",
+            "code": "&#xf1a1;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "Wordpress",
+            "code": "&#xf19a;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "Bitbucket",
+            "code": "&#xf171;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "Pinterest",
+            "code": "&#xf0d2;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "Skype",
+            "code": "&#xf17e;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "Steam",
+            "code": "&#xf1b6;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "Deviantart",
+            "code": "&#xf1bd;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        },
+        {
+            "name": "LinkedIn",
+            "code": "&#xf0e1;",
+            "styles": [
+                {
+                    "color": ""
+                }
+            ]
+        }
+    ];
 
+    public selectedIcon = {};
+
+    constructor(private screenService: BagPiScreenService) {
+        this.selectedIcon = this.iconList[0];
+    }
+
+    overrideSelected(value) {
+        for (var i = 0; i < this.iconList.length; i++) {
+            if (this.iconList[i].name == value) {
+                this.selectedIcon = this.iconList[i];
+            }
+        }
     }
 }
